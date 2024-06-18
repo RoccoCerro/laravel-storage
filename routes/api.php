2d9e1eb2c387a9505f392736e715d07f887cb322
RoccoCerro <rcerro02@gmail.com>
2024-06-18 11:58:07 +0200

    Add: mail message

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{project:slug}', [ProjectController::class, 'show']);
+Route::post('/contacts', [LeadController::class, 'store']);
